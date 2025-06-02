dd601143c47d9f94656ad7362e1ae6075a9b3c1d
Paul <paulrodrigoguasace@gmail.com>
2025-06-01 20:46:14 -1100

    Implementado rutas protegidas a todos los endpoint

--- src/main/java/com/universidad/tecno/api_gestion_accesorios/auth/SpringSecurityConfig.java
+++ src/main/java/com/universidad/tecno/api_gestion_accesorios/auth/SpringSecurityConfig.java
@@ -37,99 +37,115 @@ public class SpringSecurityConfig {
     }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(configurationSource()))
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authz -> authz
-                .requestMatchers(HttpMethod.GET, "/api/sales").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/purchases").permitAll()
-                // CLIENTS
-                .requestMatchers(HttpMethod.GET, "/api/clients/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/clients/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/clients/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/clients").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/clients/{id}").permitAll()
-                 // SUPPLIER
-                .requestMatchers(HttpMethod.GET, "/api/suppliers/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/suppliers/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/suppliers/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/suppliers").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/suppliers/{id}").permitAll()
-                // CATEGORIES
-                .requestMatchers(HttpMethod.GET, "/api/categories/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/categories").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/categories/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/categories/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/categories").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/categories/{id}").permitAll()
-                // WAREHOUSES
-                .requestMatchers(HttpMethod.GET, "/api/warehouses/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/warehouses").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/warehouses/details").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/warehouses/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/warehouses/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/warehouses").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/warehouses/{id}").permitAll()
-                // ACCESORIES
-                .requestMatchers(HttpMethod.GET, "/api/accessories/page/{page}").permitAll()
+        return http.authorizeHttpRequests(authz -> authz
+                // Rutas públicas
                 .requestMatchers(HttpMethod.GET, "/api/accessories/page/catalog/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/accessories/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/accessories/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/accessories").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/accessories/{id}").permitAll()
-                // USERS
-                
-                .requestMatchers(HttpMethod.GET, "/api/users/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/users/{id}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/users/with-permissions/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/users/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/users/change-password").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").permitAll()
-                // SALES
-                .requestMatchers(HttpMethod.GET, "/api/sales/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/sales/{id}").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/sales").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/sales/{id}").permitAll()
-                // PURCHASES
-                .requestMatchers(HttpMethod.GET, "/api/purchases/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/purchases/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/purchases").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/purchases/{id}").permitAll()
-                // ADJUSTMENTS
-                .requestMatchers(HttpMethod.GET, "/api/adjustments/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/adjustments/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/adjustments").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/adjustments/{id}").permitAll()
-                // TRANSFERSS
-                .requestMatchers(HttpMethod.GET, "/api/transfers/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/transfers/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/transfers").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/transfers/{id}").permitAll()
-                // PERMISSIONS
-                .requestMatchers(HttpMethod.GET, "/api/permissions/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/permissions").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/permissions/{id}").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/permissions/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/permissions").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/permissions/assign-to-role").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/permissions/assign-to-user").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/permissions/{id}").permitAll()
-                // ROLES
-                .requestMatchers(HttpMethod.GET, "/api/roles/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/roles//with-permissions").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/roles/with-permissions/page/{page}").permitAll()
-                .requestMatchers(HttpMethod.GET, "/api/roles/{id}").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/roles").permitAll()
-                .requestMatchers(HttpMethod.PUT, "/api/roles/{id}").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/roles/{id}").permitAll()
 
-                        .anyRequest().authenticated())
+
+                //-----------------------RUTAS PROTEGIDAS------------------------------------------
+
+                // USERS
+                .requestMatchers(HttpMethod.GET, "/api/users/page/{page}").hasAuthority("VER_USUARIO")
+                .requestMatchers(HttpMethod.GET, "/api/users/with-permissions/{id}").hasAuthority("VER_USUARIO")
+                .requestMatchers(HttpMethod.GET, "/api/users/{id}").hasAuthority("VER_USUARIO")
+                .requestMatchers(HttpMethod.POST, "/api/users").hasAuthority("CREAR_USUARIO")
+                .requestMatchers(HttpMethod.POST, "/api/users/change-password").hasAuthority("CAMBIAR_PASSWORD")
+                .requestMatchers(HttpMethod.PUT, "/api/users/{id}").hasAuthority("EDITAR_USUARIO")
+                .requestMatchers(HttpMethod.DELETE, "/api/users/{id}").hasAuthority("ELIMINAR_USUARIO")
+                
+                 //ACCESSORIES
+                .requestMatchers(HttpMethod.GET, "/api/accessories/page/{page}").hasAuthority("VER_ACCESORIO")
+                .requestMatchers(HttpMethod.GET, "/api/accessories/{id}").hasAuthority("VER_ACCESORIO")
+                .requestMatchers(HttpMethod.POST, "/api/accessories").hasAuthority("CREAR_ACCESORIO")
+                .requestMatchers(HttpMethod.PUT, "/api/accessories/{id}").hasAuthority("EDITAR_ACCESORIO")
+                .requestMatchers(HttpMethod.DELETE, "/api/accessories/{id}").hasAuthority("ELIMINAR_ACCESORIO")
+
+                //PERMISSIONS
+                .requestMatchers(HttpMethod.GET, "/api/permissions/page/{page}").hasAuthority("VER_PERMISO")
+                .requestMatchers(HttpMethod.GET, "/api/permissions").hasAuthority("VER_PERMISO")
+                .requestMatchers(HttpMethod.GET, "/api/permissions/{id}").hasAuthority("VER_PERMISO")
+                .requestMatchers(HttpMethod.POST, "/api/permissions").hasAuthority("CREAR_PERMISO")
+                .requestMatchers(HttpMethod.POST, "/api/permissions/assign-to-user").hasAuthority("ASIGNAR_PERMISOS_A_USER")
+                .requestMatchers(HttpMethod.POST, "/api/permissions/assign-to-rol").hasAuthority("ASIGNAR_PERMISOS_A_ROL")
+                .requestMatchers(HttpMethod.PUT, "/api/permissions/{id}").hasAuthority("EDITAR_PERMISO")
+                .requestMatchers(HttpMethod.DELETE, "/api/permissions/{id}").hasAuthority("ELIMINAR_PERMISO")
+
+                // CLIENTS
+                .requestMatchers(HttpMethod.GET, "/api/clients/page/{page}").hasAuthority("VER_CLIENTE")
+                .requestMatchers(HttpMethod.GET, "/api/clients/{id}").hasAuthority("VER_CLIENTE")
+                .requestMatchers(HttpMethod.POST, "/api/clients").hasAuthority("CREAR_CLIENTE")
+                .requestMatchers(HttpMethod.PUT, "/api/clients/{id}").hasAuthority("EDITAR_CLIENTE")
+                .requestMatchers(HttpMethod.DELETE, "/api/clients/{id}").hasAuthority("ELIMINAR_CLIENTE")
+
+                 // SUPPLIER
+                .requestMatchers(HttpMethod.GET, "/api/suppliers/page/{page}").hasAuthority("VER_PROVEEDOR")
+                .requestMatchers(HttpMethod.GET, "/api/suppliers/{id}").hasAuthority("VER_PROVEEDOR")
+                .requestMatchers(HttpMethod.POST, "/api/suppliers").hasAuthority("CREAR_PROVEEDOR")
+                .requestMatchers(HttpMethod.PUT, "/api/suppliers/{id}").hasAuthority("EDITAR_PROVEEDOR")
+                .requestMatchers(HttpMethod.DELETE, "/api/suppliers/{id}").hasAuthority("ELIMINAR_PROVEEDOR")
+
+                // CATEGORIES
+                .requestMatchers(HttpMethod.GET, "/api/categories/page/{page}").hasAuthority("VER_CATEGORIA")
+                .requestMatchers(HttpMethod.GET, "/api/categories").hasAuthority("VER_CATEGORIA")
+                .requestMatchers(HttpMethod.GET, "/api/categories/{id}").hasAuthority("VER_CATEGORIA")
+                .requestMatchers(HttpMethod.PUT, "/api/categories/{id}").hasAuthority("EDITAR_CATEGORIA")
+                .requestMatchers(HttpMethod.POST, "/api/categories").hasAuthority("CREAR_CATEGORIA")
+                .requestMatchers(HttpMethod.DELETE, "/api/categories/{id}").hasAuthority("ELIMINAR_CATEGORIA")
+
+                // WAREHOUSES
+                .requestMatchers(HttpMethod.GET, "/api/warehouses/page/{page}").hasAuthority("VER_ALMACEN")
+                .requestMatchers(HttpMethod.GET, "/api/warehouses").hasAuthority("VER_ALMACEN")
+                .requestMatchers(HttpMethod.GET, "/api/warehouses/details").hasAuthority("VER_ALMACEN")
+                .requestMatchers(HttpMethod.GET, "/api/warehouses/{id}").hasAuthority("VER_ALMACEN")
+                .requestMatchers(HttpMethod.PUT, "/api/warehouses/{id}").hasAuthority("EDITAR_ALMACEN")
+                .requestMatchers(HttpMethod.POST, "/api/warehouses").hasAuthority("CREAR_ALMACEN")
+                .requestMatchers(HttpMethod.DELETE, "/api/warehouses/{id}").hasAuthority("ELIMINAR_ALMACEN")
+
+                // SALES
+                .requestMatchers(HttpMethod.GET, "/api/sales/page/{page}").hasAuthority("VER_VENTA")
+                .requestMatchers(HttpMethod.GET, "/api/sales/{id}").hasAuthority("VER_VENTA")
+                //.requestMatchers(HttpMethod.POST, "/api/sales").hasAuthority("CREAR_VENTA")
+                .requestMatchers(HttpMethod.DELETE, "/api/sales/{id}").hasAuthority("ELIMINAR_VENTA")
+
+                // PURCHASES
+                .requestMatchers(HttpMethod.GET, "/api/purchases/page/{page}").hasAuthority("VER_COMPRA")
+                .requestMatchers(HttpMethod.GET, "/api/purchases/{id}").hasAuthority("VER_COMPRA")
+                .requestMatchers(HttpMethod.POST, "/api/purchases").hasAuthority("CREAR_COMPRA")
+                .requestMatchers(HttpMethod.DELETE, "/api/purchases/{id}").hasAuthority("ELIMINAR_COMPRA")
+
+                // ADJUSTMENTS
+                .requestMatchers(HttpMethod.GET, "/api/adjustments/page/{page}").hasAuthority("VER_AJUSTE")
+                .requestMatchers(HttpMethod.GET, "/api/adjustments/{id}").hasAuthority("VER_AJUSTE")
+                .requestMatchers(HttpMethod.POST, "/api/adjustments").hasAuthority("CREAR_AJUSTE")
+                .requestMatchers(HttpMethod.DELETE, "/api/adjustments/{id}").hasAuthority("ELIMINAR_AJUSTE")
+
+                // TRANSFERS
+                .requestMatchers(HttpMethod.GET, "/api/transfers/page/{page}").hasAuthority("VER_TRASPASO")
+                .requestMatchers(HttpMethod.GET, "/api/transfers/{id}").hasAuthority("VER_TRASPASO")
+                .requestMatchers(HttpMethod.POST, "/api/transfers").hasAuthority("CREAR_TRASPASO")
+                .requestMatchers(HttpMethod.DELETE, "/api/transfers/{id}").hasAuthority("ELIMINAR_TRASPASO")
+                
+                // ROLES
+                .requestMatchers(HttpMethod.GET, "/api/roles/page/{page}").hasAuthority("VER_ROL")
+                .requestMatchers(HttpMethod.GET, "/api/roles//with-permissions").hasAuthority("VER_ROL")
+                .requestMatchers(HttpMethod.GET, "/api/roles/with-permissions/page/{page}").hasAuthority("VER_ROL")
+                .requestMatchers(HttpMethod.GET, "/api/roles/{id}").hasAuthority("VER_ROL")
+                .requestMatchers(HttpMethod.POST, "/api/roles").hasAuthority("CREAR_ROL")
+                .requestMatchers(HttpMethod.PUT, "/api/roles/{id}").hasAuthority("EDITAR_ROL")
+                .requestMatchers(HttpMethod.DELETE, "/api/roles/{id}").hasAuthority("ELIMINAR_ROL")
+
+
+
+
+                // Otros endpoints pueden agregarse igual
+                .anyRequest().authenticated())
+                .cors(cors -> cors.configurationSource(configurationSource()))
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-                .addFilter(new JwtValidationFilter(authenticationManager()));
-        return http.build();
+                .addFilter(new JwtValidationFilter(authenticationManager()))
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
     @Bean
     CorsConfigurationSource configurationSource() {
